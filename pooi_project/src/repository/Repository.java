a12f35a370b7db52446758850035b92267798cf5
Pedro Henrique <phenrique.guerra@hotmail.com>
2023-08-17 18:57:17 -0300

    Criada a classe Abstract do repository

--- pooi_project/src/repository/Repository.java
+++ pooi_project/src/repository/Repository.java
@@ -1,2 +1,15 @@
-package repository;public class Repository {
+package repository;
+
+import java.util.List;
+
+public abstract class Repository {
+
+    List<Object> lista;
+
+    public abstract void add(Object obj);
+
+    public abstract Object get(String nome);
+
+    public abstract List<Object> findAll();
+
 }
